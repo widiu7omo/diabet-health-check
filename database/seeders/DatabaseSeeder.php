d2a7b11e52c5818a7b049ace0559d13af4374b67
Danar Widi <widiu7omo@gmail.com>
2022-07-12 00:44:48 +0800

    Init Commit - Add User & Roles

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::create([
+            'name' => 'Admin',
+            'email' => 'admin@adm.com',
+            'password' => bcrypt('admin'),
+            ]);
+    }
+}
---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,16 @@
+<li class="nav-item">
+    <a href="{{ route('roles.index') }}"
+       class="nav-link {{ Request::is('roles*') ? 'active' : '' }}">
+        <p>Roles</p>
+    </a>
+</li>
+
+
+<li class="nav-item">
+    <a href="{{ route('users.index') }}"
+       class="nav-link {{ Request::is('users*') ? 'active' : '' }}">
+        <p>Users</p>
+    </a>
+</li>
+
+
