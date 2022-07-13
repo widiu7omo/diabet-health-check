c8bf8445ba5fde271d1427a8e40ca8c450136b44
Danar Widi <widiu7omo@gmail.com>
2022-07-13 17:03:39 +0800

    Add local notification

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -3,17 +3,42 @@ import 'package:diabetesapps/services/rest_http_service.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'firebase_options.dart';
 
+late AndroidNotificationChannel channel;
+late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+void onIOSDidReceiveLocalNotification(
+    int id, String? title, String? body, String? payload) async {
+  print(title);
+  print(body);
+}
+
 void main() async {
   //TODO: get local storage token
   await WidgetsFlutterBinding.ensureInitialized();
+  channel = const AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    description: 'This channel is used for important notifications.',
+    // description
+    importance: Importance.max,
+  );
+  var initializationSettingsAndroid = AndroidInitializationSettings(
+      '@mipmap/ic_launcher'); // <- default icon name is @mipmap/ic_launcher
+  var initializationSettingsIOS = IOSInitializationSettings(
+      onDidReceiveLocalNotification: onIOSDidReceiveLocalNotification);
+  var initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+  flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  flutterLocalNotificationsPlugin.initialize(initializationSettings);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   runApp(MyApp());
 }
 
@@ -32,7 +57,6 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   late String? tokenApi;
   RestHttpService? httpService = null;
-
   @override
   void initState() {
     getToken().then((token) {
@@ -41,9 +65,7 @@ class _MyAppState extends State<MyApp> {
         print(tokenApi);
         httpService = RestHttpService.create(bearerToken: tokenApi ?? "");
         try {
-          final response =
-              await httpService?.postTokenFCM(body: {"tokenFCM": tokenFcm});
-          print(response?.body);
+          await httpService?.postTokenFCM(body: {"tokenFCM": tokenFcm});
         } catch (e) {
           print(e);
         }
@@ -59,9 +81,39 @@ class _MyAppState extends State<MyApp> {
   }
 
   Future<String?> firebaseInit() async {
-    await FirebaseMessaging.instance.requestPermission();
+    await FirebaseMessaging.instance.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
     String? tokenFCM = await FirebaseMessaging.instance.getToken();
     print(tokenFCM);
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+
+      // If `onMessage` is triggered with a notification, construct our own
+      // local notification to show to users using the created channel.
+      if (notification != null && android != null) {
+        flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+                android: AndroidNotificationDetails(
+                    'high_important_channel', 'Diabetes Health Check',
+                    channelDescription:
+                        'This channel only for Diabetes Health Check',
+                    importance: Importance.max,
+                    priority: Priority.max,
+                    icon: android.smallIcon,
+                    ticker: 'ticker')));
+      }
+    });
     return tokenFCM;
   }
 
