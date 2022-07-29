c54d1140f6f32d566e3a27379cfad0e5cc7493ae
Danar Widi <widiu7omo@gmail.com>
2022-07-29 08:49:30 +0800

    Update edit profile

--- backend/app/Http/Controllers/API/AuthAPIController.php
+++ backend/app/Http/Controllers/API/AuthAPIController.php
@@ -70,9 +70,24 @@ class AuthAPIController extends AppBaseController
     public function updateProfile(Request $request)
     {
         if ($request->user()->hasRole("Pasien")) {
-            return $this->sendResponse($request->user()->only(["name", 'email', 'email_kerabat', 'phone', 'alamat', 'tempat_lahir', 'tanggal_lahir']), 'success');
+            $this->userRepository->update($request->all(), $request->user()->id);
+            return $this->sendResponse($request->user()->only(['id', "name", 'email', 'email_kerabat', 'phone', 'alamat', 'tempat_lahir', 'tanggal_lahir']), 'success');
         } else {
-            return $this->sendResponse([], 'failed');
+            return $this->sendError("You dont have permission to change password");
+        }
+    }
+
+    public function changePassword(Request $request)
+    {
+        if ($request->user()->hasRole("Pasien")) {
+            if (Hash::check($request->user()->password, $request->old_password)) {
+                $this->userRepository->update(['password' => $request->new_password], $request->user()->id);
+                return $this->sendResponse([], 'success');
+            } else {
+                return $this->sendError("Failed Change Password");
+            }
+        } else {
+            return $this->sendError("You dont have permission to change password");
         }
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -21,6 +21,7 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::post('/post_token_FCM', [App\Http\Controllers\API\AuthAPIController::class, 'postTokenFCM']);
     Route::get('/user', [App\Http\Controllers\API\AuthAPIController::class, 'user']);
     Route::post('/user', [App\Http\Controllers\API\AuthAPIController::class, 'updateProfile']);
+    Route::post('/changePassword', [App\Http\Controllers\API\AuthAPIController::class, 'changePassword']);
     Route::resource('users', App\Http\Controllers\API\UserAPIController::class);
     Route::resource('jadwal_checkups', App\Http\Controllers\API\JadwalCheckupAPIController::class);
     Route::resource('pemeriksaans', App\Http\Controllers\API\PemeriksaanAPIController::class);
